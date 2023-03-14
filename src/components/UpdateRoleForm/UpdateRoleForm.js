85be39ea2fa47841fab94e19e88bb5f99d517b5a
Erm <ermithon@gmail.com>
2023-03-14 12:01:02 -0400

    re-render test

--- src/components/UpdateRoleForm/UpdateRoleForm.js
+++ src/components/UpdateRoleForm/UpdateRoleForm.js
@@ -42,7 +42,6 @@ export default function UpdateRoleForm({ data }) {
   async function handleSubmit(event) {
     event.preventDefault();
     await rolesAPI.updateRole(role, roleId);
-    <Link to={`/roles/${role.jobTitle}`} state={{ data: { role } }}></Link>
   }
 
   async function handleDelete() {
